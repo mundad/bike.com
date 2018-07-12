0f36fc60776803c371d3086f92ab2fb948fc60d9
Munirjon Dadojonov <mundad@yandex.ru>
2018-07-12 12:20:51 +0500

    11391

--- front-end/bike.io/php/get_prices.php
+++
@@ -1,4 +0,0 @@
-<?php
-$array=['a_b_hrs'=>'10','a_b_day'=>'50','k_b_hrs'=>'7',
-    'k_b_day'=>'35','t_b_hrs'=>'15','t_b_day'=>'75','helmet'=>'5','lock'=>'3','basket'=>'4','baby_seat'=>'7'];
-echo json_encode($array);
