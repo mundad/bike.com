67bb09ef8bd9dce7107f9bf359a28a33aec12003
Munirjon Dadojonov <mundad@yandex.ru>
2018-07-13 14:38:18 +0500

    143813072018

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -19,26 +19,43 @@ class UserTableSeeder extends Seeder
         $employee->login= 'admin';
         $employee->name = 'Admin Name';
         $employee->email = 'emp@e';
-        $employee->password = bcrypt('1');
+        $employee->password = bcrypt('735700');
         $employee->save();
         $employee->roles()->attach($role_employee);
         $employee->roles()->attach($role_manager);
 
         $manager = new User();
-            $manager->login = 'manger';
+            $manager->login = 'cashier1';
         $manager->name = 'manger Name';
         $manager->email = 'manager@example.com';
-        $manager->password = bcrypt('1');
+        $manager->password = bcrypt('735701');
         $manager->save();
         $manager->roles()->attach($role_manager);
 
         $manager = new User();
-        $manager->login = 'manger2';
+        $manager->login = 'cashier2';
         $manager->name = 'manger Name';
         $manager->email = 'manager2@example.com';
-        $manager->password = bcrypt('1');
+        $manager->password = bcrypt('735702');
         $manager->save();
         $manager->roles()->attach($role_manager);
+
+        $manager = new User();
+        $manager->login = 'cashier3';
+        $manager->name = 'manger Name';
+        $manager->email = 'manager@example.com';
+        $manager->password = bcrypt('735703');
+        $manager->save();
+        $manager->roles()->attach($role_manager);
+
+        $manager = new User();
+        $manager->login = 'cashier4';
+        $manager->name = 'manger Name';
+        $manager->email = 'manager2@example.com';
+        $manager->password = bcrypt('735704');
+        $manager->save();
+        $manager->roles()->attach($role_manager);
+
 
         //
     }
